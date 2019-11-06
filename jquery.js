8b09068be2e3752cd27354742f9f2308f045627c
keuapar <mikulas.plesak@gmail.com>
2019-11-07 00:25:05 +0900

    functionality

--- jquery.js
+++ jquery.js
@@ -7,11 +7,20 @@ $(document).ready(function () {
     $('.line1').fadeIn(1000).removeClass('hidden');
     */
 
-    // choice redirect (put url as div parameter)
-    $('.choice').click(function () {
-        window.location = $(this).attr('url');
-        return false
-    })
+    // scroll to choices
+    var once = true;
+    $(window).scroll(function () {
+        var hT = $('.choice-screen').offset().top,
+            hH = $('.choice-screen').outerHeight(),
+            wH = $(window).height(),
+            wS = $(this).scrollTop();
+            //console.log(wS, lastScrollTop);
+        if (wS + wH >= hT && once) {
+            var bottom = hT + hH;
+            $('html, body').animate({scrollTop:bottom}, 3000);
+            once = false;
+        }
+    });
 
     // start game
     $('.GO-button').click(function () {
@@ -20,18 +29,36 @@ $(document).ready(function () {
         Cookies.set('door', 'look');
         Cookies.set('plan', 'considering');
         Cookies.set('sure', 'perhaps');
+        window.location = $(this).attr('url');
+        return false
+        // blackout the screen
     });
+
+    // choice redirect (put url as div parameter)
+    $('.choice').click(function () {
+        window.location = $(this).attr('url');
+        return false
+    })
 
     // switch map and back to game
     $('.map-button').click(function () {
         // switch from GAME to MAP
-        if($('.map-button-text').text() == 'MAP'){
+        if ($('.map-button-text').text() == 'MAP') {
             $('.map-button-text').text('BACK');
-            $('.map').css({visibility: 'visible', opacity: 0}).animate({opacity: 1}, 300);
+            $('.map').css({
+                visibility: 'visible',
+                opacity: 0
+            }).animate({
+                opacity: 1
+            }, 300);
             //$('.map').css('visibility', 'visible');
         } else { // switch from MAP to GAME
             $('.map-button-text').text('MAP');
-            $('.map').css({opacity: 1}).animate({opacity: 0}, 300, function() {
+            $('.map').css({
+                opacity: 1
+            }).animate({
+                opacity: 0
+            }, 300, function () {
                 $('.map').css('visibility', 'hidden');
             });
             //$('.map').css('visibility', 'hidden');
