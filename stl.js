4509b178dc600e97c0e44345903c3ab7a96bbe00
keuapar <mikulas.plesak@gmail.com>
2019-11-29 20:46:26 +0900

    model works

--- stl.js
+++ stl.js
@@ -3,10 +3,10 @@ $(document).ready(function () {
     // stl_viewer
     if ($('#stl_cont').length) {
         var stl_viewer = new StlViewer(
-            $('#stl_cont'), {
-                models: [{
-                    filename: 'house1.stl'
-                }]
+            $('#stl_cont')[0], {
+                models: {
+                    id: 55, filename: '_1.stl', display: 'smooth', rotationx: -0.75
+                }
             }
         );
         //stl_viewer.add_model({id: 55, filename: 'house1.stl'});
