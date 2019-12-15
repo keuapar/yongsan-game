a043d1574b68c3c44904c3bb0169012aa5bf41c0
keuapar <mikulas.plesak@gmail.com>
2019-12-15 21:49:42 +0900

    correct files in incorrect positions

--- stl.js
+++ stl.js
@@ -113,16 +113,56 @@ $(document).ready(function () {
                 console.log($('#stl_cont').attr('data-map'));
                 var stl_viewer = new StlViewer(
                     $('#stl_cont')[0], {
-                        models: {
-                            id: 55,
-                            filename: 'Samgakji_20mb.stl',
-                            x: 0,
-                            y: 0,
-                            z: 0,
+                        models: [{
+                            id: 22,
+                            filename: 'sam_road.stl',
                             display: 'smooth',
+                            color: '#909090',
                             rotationx: -1.5,
                             rotationy: -0.2
-                        }
+                        }, {
+                            id: 33,
+                            filename: 'sam_rot.stl',
+                            display: 'smooth',
+                            color: '#333333',
+                            rotationx: -1.5,
+                            rotationy: -0.2
+                        }, {
+                            id: 44,
+                            filename: 'sam_res.stl',
+                            display: 'smooth',
+                            color: '#b84848',
+                            rotationx: -1.5,
+                            rotationy: -0.2
+                        }, {
+                            id: 55,
+                            filename: 'sam_daeg.stl',
+                            display: 'smooth',
+                            color: '#4860b8',
+                            rotationx: -1.5,
+                            rotationy: -0.2
+                        }, {
+                            id: 66,
+                            filename: 'sam_art.stl',
+                            display: 'smooth',
+                            color: '#489ab8',
+                            rotationx: -1.5,
+                            rotationy: -0.2
+                        }, {
+                            id: 77,
+                            filename: 'sam_fac.stl',
+                            display: 'smooth',
+                            color: '#b88b48',
+                            rotationx: -1.5,
+                            rotationy: -0.2
+                        }, {
+                            id: 88,
+                            filename: 'sam_man.stl',
+                            display: 'smooth',
+                            color: '#b848a7',
+                            rotationx: -1.5,
+                            rotationy: -0.2
+                        }]
                     }
                 );
                 break;
